45d5c1e3cb8080a0e07243a56cdf32ed79778c7b
Erik Muro <erikmuro@Eriks-iMac.local>
2016-12-10 14:06:09 -0600

    updated image emit calll

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ var options = {
 };
 var server = https.createServer(options, app);
 var io = require('socket.io')(server);
+var ss = require('socket.io-stream');
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+
+    // var clients = io.of('/chat').clients();
     console.log('a user connected');
     socket.on('disconnect', function(){
         console.log('user disconnected');
@@ -20,6 +23,15 @@ io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
      });
+     socket.on('imageBuffer', function(imageBuffer){
+         console.log("getting stream from video");
+        //  console.log(imageBuffer);
+         io.emit('image', imageBuffer);
+      });
+    //   ss(socket).on('imageBuffer', function(stream) {
+    //       console.log("getting ss from video");
+    //     io.emit('image', stream);
+    //   });
 });
 
 server.listen(443, function(){
