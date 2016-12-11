c5040f5b2931b0a6eae889c17f6d0dc329002563
Erik Muro <erikmuro@Eriks-iMac.local>
2016-12-11 15:40:31 -0600

    working example with image streaming video

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var fs = require('fs')
 var app = require('express')();
 var https = require('https');
 var options = {
-  key: fs.readFileSync('./config/local.key'),
-  cert: fs.readFileSync('./config/local.crt')
+    key: fs.readFileSync('./config/local.key'),
+    cert: fs.readFileSync('./config/local.crt')
 };
 var server = https.createServer(options, app);
 var io = require('socket.io')(server);
@@ -22,16 +22,23 @@ io.on('connection', function(socket){
     });
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
-     });
-     socket.on('imageBuffer', function(imageBuffer){
-         console.log("getting stream from video");
-        //  console.log(imageBuffer);
-         io.emit('image', imageBuffer);
-      });
+    });
+    socket.on('imageBuffer', function(imageBuffer){
+        console.log("getting stream from video");
+
+        io.emit('image', imageBuffer);
+        // io.emit('test', "hello");
+        // console.log(imageBuffer);
+        // require("fs").createWriteStream("./result.jpg").end(imageBuffer);
+    });
     //   ss(socket).on('imageBuffer', function(stream) {
     //       console.log("getting ss from video");
     //     io.emit('image', stream);
     //   });
+    socket.on('get image', function(){
+        io.emit('get image');
+        // console.log("requesting image");
+    });
 });
 
 server.listen(443, function(){
